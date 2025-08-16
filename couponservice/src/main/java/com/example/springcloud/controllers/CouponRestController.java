878b51f39849577a2862f7b060aaf6f4e1a5f5f0
Saranya Gunasekaran <sarangunasns@gmail.com>
2025-08-15 22:21:29 -0700

    Update Unit tests for security

--- couponservice/src/main/java/com/example/springcloud/controllers/CouponRestController.java
+++ couponservice/src/main/java/com/example/springcloud/controllers/CouponRestController.java
@@ -1,6 +1,7 @@
 package com.example.springcloud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,22 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springcloud.model.Coupon;
 import com.example.springcloud.repos.CouponRepo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/couponapi")
+@CrossOrigin
 public class CouponRestController {
 
     @Autowired
     CouponRepo couponRepo;
 
     @PostMapping("/coupons")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public Coupon createCoupon(@RequestBody Coupon coupon) {
         // Logic to create a coupon
         return couponRepo.save(coupon);
     }
 
     @GetMapping("/coupons/{code}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public Coupon getCoupon(@PathVariable("code") String code) {
         // Logic to retrieve a coupon by code
         return couponRepo.findByCode(code);
--- couponservice/src/test/java/com/example/springcloud/couponservice/CouponserviceApplicationTests.java
+++ couponservice/src/test/java/com/example/springcloud/couponservice/CouponserviceApplicationTests.java
@@ -1,13 +1,84 @@
 package com.example.springcloud.couponservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CouponserviceApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	public void testGetCouponWithoutAuth_forbidden() throws Exception{
+		mvc.perform(get("/couponapi/coupons/SUPERSALE")).andExpect(status().isUnauthorized());
 	}
+
+	@Test
+	//@WithMockUser(roles = {"ADMIN"}) 
+	@WithUserDetails("doug@bailey.com")
+	public void testGetCouponWithoutAuth_ok() throws Exception{
+		mvc.perform(get("/couponapi/coupons/SUPERSALE")).andExpect(status().isOk())
+		.andExpect(content().string("{\"id\":1,\"code\":\"SUPERSALE\",\"discount\":10.00,\"expDate\":\"12/12/2020\"}"));
+	}
+
+	@Test
+	@WithMockUser(roles = {"ADMIN"})
+	public void testCreateCoupon_WithoutCsrf_Forbidden() throws Exception {
+		mvc.perform(
+			post("/saveCoupon")
+				.content("{\"code\":\"SUPERSALECSRF\",\"discount\":70.00,\"expDate\":\"12/12/2020\"}")
+				.contentType("application/json")
+		)
+		.andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithMockUser(roles = {"ADMIN"})
+	public void testCreateCoupon_WithCsrf_Ok() throws Exception {
+		mvc.perform(
+			post("/saveCoupon")
+				.content("{\"code\":\"SUPERSALECSRF\",\"discount\":70.00,\"expDate\":\"12/12/2020\"}")
+				.contentType("application/json").with(csrf().asHeader())
+		)
+		.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockUser(roles = {"USER"})
+	public void testCreateCoupon_NonAdminUser_Forbidden() throws Exception {
+		mvc.perform(
+			post("/saveCoupon")
+				.content("{\"code\":\"SUPERSALECSRF\",\"discount\":70.00,\"expDate\":\"12/12/2020\"}")
+				.contentType("application/json").with(csrf().asHeader())
+		)
+		.andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithMockUser(roles = {"ADMIN"})
+	public void testCORS() throws Exception {
+		mvc.perform(options("/couponapi/coupons").header("Access-Control-Request-Method", "POST").
+		header("Origin", "http://www.google.com")).andExpect(status().isOk())
+			.andExpect(header().exists("Access-Control-Allow-Origin"))
+			.andExpect(header().string("Access-Control-Allow-Origin", "*"))
+			.andExpect(header().exists("Access-Control-Allow-Methods"))
+			.andExpect(header().string("Access-Control-Allow-Methods", "POST"));
+		
+	}
+	
 
 }
